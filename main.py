92ebee907dacf0c920366e07b19eaad15e4a290a
Erik Fredericks <frederer@gvsu.edu>
2022-11-23 15:16:52 -0500

    anim

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprite_object import *
+from object_handler import *
 
 class Game:
     def __init__(self):
@@ -29,10 +31,14 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.object_handler = ObjectHandler(self)
+        # self.static_sprite = SpriteObject(self)
+        # self.animated_sprite = AnimatedSprite(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.object_handler.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() : 0.1f}')
